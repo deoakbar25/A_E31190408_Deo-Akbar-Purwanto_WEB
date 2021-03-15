fe639d081a0e8fa7b28c60b384a172a9ac419c3c
deoakbar25 <65730639+deoakbar25@users.noreply.github.com>
2021-03-15 22:14:05 +0700

    Tugas Minggu 5

--- Latihan.php
+++ Latihan.php
@@ -1,73 +1,75 @@
-<?php 
+<?php
 
-class Kalkulator {
-    private $var_a = 0, $var_b = 0, $var_c = 0;
+interface hitungLuas {
+    public function hitungLuasPersegi($sisi);
 
-    public function setBilangan1( $var_a ) {
-        $this->var_a = $var_a;
-    }
+    public function hitungLuasSegitiga($sisi);
 
-    public function getBilangan1() {
-        return $this->var_a;
-    }
-
-    public function setBilangan2( $var_b ) {
-        $this->var_b = $var_b;
-    }
-
-    public function getBilangan2() {
-        return $this->var_b;
-    }
-
-    public function setBilangan3( $var_c ) {
-        $this->var_c = $var_c;
-    }
-
-    public function getBilangan3() {
-        return $this->var_c;
-    }
-
-    public function tambah() {
-        $hasil = $this->var_a + $this->var_b + $this->var_c;
-        return "{$this->var_a} + {$this->var_b} + {$this->var_c} = <b>".$hasil."</b>";
-    }
-
-    public function kurang() {
-        $hasil = $this->var_a - $this->var_b - $this->var_c;
-        return "{$this->var_a} - {$this->var_b} - {$this->var_c} = <b>".$hasil."</b>";
-    }
-
-    public function bagi() {
-        $hasil = $this->var_a / $this->var_b / $this->var_c;
-        return "{$this->var_a} : {$this->var_b} : {$this->var_c} = <b>".$hasil."</b>";
-    }
-
-    public function kali() {
-        $hasil = $this->var_a * $this->var_b * $this->var_c;
-        return "{$this->var_a} &times; {$this->var_b} &times; {$this->var_c} = <b>".$hasil."</b>";
-    }
-
+    public function hitungLuasLingkaran($sisi);
 }
 
-$bilangan = new Kalkulator();
+class Persegi implements hitungLuas {
 
-$bilangan->setBilangan1(50);
-$bilangan->setBilangan2(20);
-$bilangan->setBilangan3(5);
+    public function hitungLuasPersegi($sisi)
+    {
+        return $sisi * $sisi;
+    }
 
-echo "PERHITUNGAN BILANGAN : <b>{$bilangan->getBilangan1()}</b>, <b>{$bilangan->getBilangan2()}</b>, <b>{$bilangan->getBilangan3()}</b> <br><br>";
+    public function hitungLuasSegitiga($sisi)
+    {
+        return "Error : Objek bukan segitiga.";
+    }
 
-echo '<b>Penjumlahan :</b><br>';
-echo $bilangan->tambah();
-echo '<br><br>';
+    public function hitungLuasLingkaran($sisi)
+    {
+        return "Error : Objek bukan Lingkaran";
+    }
+}
 
-echo '<b>Pengurangan :</b><br>';
-echo $bilangan->kurang();
-echo '<br><br>';
+class Segitiga implements hitungLuas {
 
-echo '<b>Pembagian :</b><br>';
-echo $bilangan->bagi();
-echo '<br><br>';
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukan persegi";
+    }
 
-echo '<b>Perkalian :</b><br>';
-echo $bilangan->kali();
+    public function hitungLuasSegitiga($sisi)
+    {
+        return (1/2) * $sisi * $sisi;
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return "Error : Objek bukan lingkaran";
+    }
+}
+
+class Lingkaran implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukan persegi";
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return "Error : Objek bukan segitiga";
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return $sisi * $sisi * pi();
+    }
+}
+
+$objek1 = new Persegi();
+$objek2 = new Segitiga();
+$objek3 = new Lingkaran();
+
+echo "<b>Jika luas sisi = 3</b><hr>";
+echo "Luas Persegi = {$objek1->hitungLuasPersegi(3)}";
+echo '<br>';
+echo "Luas Segitiga = 
+{$objek2->hitungLuasSegitiga(3)}";
+echo '<br>';
+echo "Luas Lingkaran = {$objek3->hitungLuasLingkaran(3)}";
--- Praktikum2.php
+++ Praktikum2.php
@@ -1,25 +1,15 @@
 <?php
-class Car {
-    //private
-    private $model;
-    public function getModel()
-    {
-        return "The car model is " . $this->model;
-    }
 
-    //perbaikan 'menambahkan fungsi setter untuk mengakses Property dengan Visibility Private'
-    public function setModel($model){
-        $this->model = $model;
-    }
+class miniCar implements Car {
+	private $model;
+
+	public function setModel($name)
+	{
+		$this -> model = $name;
+	}
+	public function getModel()
+	{
+		return $this -> model;
+	}
 }
-
-$mercedes = new Car();
-
-////akses property dari luar class.
-
-// $mercedes->model = "Mercedes benz"; //jika mengakses dari luar kelas akan terjadi error
-
-//perbaikan
-$mercedes->setModel("Mercedes benz");
-
-echo $mercedes->getModel();
+?>
